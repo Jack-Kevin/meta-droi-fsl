2dccbc4d2c6226c218c87eb8a75703004c4b5952
zhangwuba <zhangwuba@droi.com>
2019-02-19 14:55:09 +0800

    ctrl poweron 8183

--- recipes-core/droi-shell/files/poweron-mt8183.sh
+++ recipes-core/droi-shell/files/poweron-mt8183.sh
@@ -8,31 +8,10 @@
 # 
 #
 test "$VERBOSE" != no && echo -n "Power on MT8183..."
-#echo -n "Power on MT8183 (1)..."
-#sleep 20s
-#echo 0x0001 > /sys/devices/platform/power_rst_gpio/droi_power_rst_access
+echo -n "Power on MT8183 (1-4)..."
+echo 0x000F > /sys/devices/platform/power_rst_gpio/droi_power_rst_access
 sleep 30s
-echo -n "Power on MT8183 (2)..."
-echo 0x0002 > /sys/devices/platform/power_rst_gpio/droi_power_rst_access
-sleep 5s
-echo -n "Power on MT8183 (3)..."
-echo 0x0004 > /sys/devices/platform/power_rst_gpio/droi_power_rst_access
-sleep 5s
-echo -n "Power on MT8183 (4)..."
-echo 0x0008 > /sys/devices/platform/power_rst_gpio/droi_power_rst_access
-sleep 30s
-echo -n "Power on MT8183 (5)..."
-echo 0x0010 > /sys/devices/platform/power_rst_gpio/droi_power_rst_access
-sleep 5s
-echo -n "Power on MT8183 (6)..."
-echo 0x0020 > /sys/devices/platform/power_rst_gpio/droi_power_rst_access
-#sleep 5s
-#echo -n "Power on MT8183 (7)..."
-#echo 0x0040 > /sys/devices/platform/power_rst_gpio/droi_power_rst_access
-#sleep 5s
-#echo -n "Power on MT8183 (8)..."
-#echo 0x0080 > /sys/devices/platform/power_rst_gpio/droi_power_rst_access
-
-
+echo -n "Power on MT8183 (5-8)..."
+echo 0x00F0 > /sys/devices/platform/power_rst_gpio/droi_power_rst_access
 : exit 0
 
